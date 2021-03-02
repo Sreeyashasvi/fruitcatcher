01a7cfbefeac1c2eff1886c065648558450b740a
Sreeyashasvi <74099332+Sreeyashasvi@users.noreply.github.com>
2021-03-02 07:00:51 +0530

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
-# fruitcatcher
+# CarRacingGame1.0
+Car Racing Game Stage 1.0
---
+++ sketch.js
@@ -0,0 +1,18 @@
+var canva;
+var gameState=0;
+var constentCount;
+var database;
+var quiz;
+var question;
+var constent;
+
+function setup(){
+  canvas = createCanvas(850,400);
+}
+
+
+function draw(){
+  background("pink");
+
+  
+}
